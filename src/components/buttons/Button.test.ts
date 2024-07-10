38b54d221c772d41e1e4b1e81c61ee5bfc9d7482
glagman <150083650+glagman@users.noreply.github.com>
2024-07-10 10:43:15 -0500

    testing CI

--- src/components/buttons/Button.test.ts
+++ src/components/buttons/Button.test.ts
@@ -1,9 +1,21 @@
-import '@testing-library/jest-dom';
-import { sum } from "./Button";
-import {expect, test} from '@jest/globals';
+// import '@testing-library/jest-dom';
+// import { sum } from "./Button";
+// import {expect, test} from '@jest/globals';
  
-test('adds 1 + 2 to equal 3', () => {
-   expect(sum(1, 2)).toBe(3);
- });
+// test('adds 1 + 2 to equal 3', () => {
+//    expect(sum(1, 2)).toBe(3);
+//  });
  
+
+// button.test.ts
+import "@testing-library/jest-dom";
+import React from 'react';
+import { render } from '@testing-library/react';
+import Button from './Button';
+
+test("Button has correct background color when primary prop is true", () => {
+  const { getByText } = render(<Button primary>Click Me</Button>);
+  const button = getByText('Click Me');
+  expect(button).toHaveStyle('background-color: blue');
+});
 
